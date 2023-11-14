fedad949b4db1c745d2342f4b4dca762f6abb137
Thiago Silva <thiago.tlsbomu@gmail.com>
2023-11-14 16:45:16 -0300

    Added images control

--- admin/components/ProductForm.js
+++ admin/components/ProductForm.js
@@ -1,21 +1,26 @@
 import axios from "axios";
 import { useRouter } from "next/router";
 import { useState } from "react";
+import Spinner from "./Spinner";
+import {ReactSortable} from 'react-sortablejs';
 
 export default function ProductForm({
     _id,
     title: existingTitle, 
     description: existingDescription, 
-    price: existingPrice
+    price: existingPrice,
+    images: existingimages,
 }) {
     const[title,setTitle] = useState(existingTitle || '');
     const[description,setDescription] = useState(existingDescription || '');
     const[price,setPrice] = useState(existingPrice || '');
     const[goToProducts, setGoToProducts] = useState(false);
+    const[images, setImages] = useState(existingimages || []);
+    const[isUploading,setIsUploading] = useState(false);
     const router = useRouter();
     async function saveProduct(ev) {
         ev.preventDefault();
-        const data = {title, description, price};
+        const data = {title, description, price, images};
         if(_id) {
             await axios.put('/api/products', {...data, _id});
         } else {
@@ -26,6 +31,24 @@ export default function ProductForm({
     if(goToProducts){
         router.push('/products');
     }
+    async function uploadImages(ev){
+        const files = ev.target?.files;
+        if(files?.length >0) {
+            setIsUploading(true);
+            const data = new FormData();
+            for(const file of files) {
+                data.append('file', file);
+            }
+            const res = await axios.post('/api/upload', data);
+            setImages(oldImages => {
+                return [...oldImages, ...res.data.links]
+            })
+            setIsUploading(false);
+        }
+    }
+    function updateImagesOrder(images){
+        setImages(images);
+    }
     return (
         <form onSubmit={saveProduct}>
             <label>Product Name</label>
@@ -35,6 +58,33 @@ export default function ProductForm({
                 value={title} 
                 onChange={ev => setTitle(ev.target.value)}
             />
+            <label>Photos </label>
+            <div className="mb-2 flex flex-wrap gap-1">
+                <ReactSortable 
+                    list={images} 
+                    className="flex flex-wrap gap-1"
+                    setList={updateImagesOrder} >
+                    {!!images?.length && images.map(link => (
+                        <div key={link} className="h-24">
+                            <img src={link} alt="" className="rounded-lg"/>
+                        </div>
+                    ))}
+                </ReactSortable>
+                {isUploading && (
+                    <div className="h-24 p-1 flex items-center">
+                        <Spinner />
+                    </div>
+                )}
+                <label className="flex w-24 h-24 cursor-pointer text-center items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                    </svg>
+                    <div>
+                        Upload
+                    </div>
+                    <input type="file" onChange={uploadImages} className="hidden"/>
+                </label>
+            </div>
             <label>Description</label>
             <textarea 
                 placeholder="description"
---
+++ admin/components/Spinner.js
@@ -0,0 +1,7 @@
+import {GridLoader} from 'react-spinners';
+
+export default function Spinner() {
+    return (
+        <GridLoader color="#1E3A8A" />
+    )
+}
