dc1ffb3056d7264b8438c0128a99dfe6cae66703
Thiago Silva <thiago.tlsbomu@gmail.com>
2023-11-26 13:55:59 -0300

    Logout and security

--- admin/pages/api/auth/[...nextauth].js
+++ admin/pages/api/auth/[...nextauth].js
@@ -1,9 +1,10 @@
-import NextAuth from 'next-auth'
+import NextAuth, { getServerSession } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import { MongoDBAdapter } from '@auth/mongodb-adapter'
 import clientPromise from '@/lib/mongodb'
 
-export default NextAuth({
+const adminEmails = ['thiago.tlsbom@gmail.com'];
+export const authOptions = {
   providers: [
     // OAuth authentication providers...
     GoogleProvider({
@@ -12,4 +13,23 @@ export default NextAuth({
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
-})
+  callbacks: {
+    session: ({session, token, user}) => {
+      if(adminEmails.includes(session?.user?.email)) {
+        return session;
+      }else {
+        return false;
+      }
+    }
+  }
+}
+export default NextAuth(authOptions);
+
+export async function isAdminRequest(req, res) {
+  const session = await getServerSession(req, res, authOptions);
+  if(!adminEmails.includes(session?.user?.email)) {
+    res.status(401);
+    res.end();
+    throw 'not an admin';
+  }
+}
